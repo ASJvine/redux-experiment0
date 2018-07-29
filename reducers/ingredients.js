7a719b6c39894a4459019aad04e04c1f86ad3940
Alexandre Serra Jaumot <aserrajaumot@gmail.com>
2018-07-29 18:24:44 +0200

    Moving action types to a file constants/actionTypes

--- reducers/ingredients.js
+++ reducers/ingredients.js
@@ -1,8 +1,10 @@
+import { ADD_INGREDIENT } from '../constants/actionTypes';
+
 const initialState = [{ recipe: 'Omelette', name: 'Egg', quantity: 2 }];
 
 const ingredientsReducer = (ingredients = initialState, action) => {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       const newIngredient = {
             name: action.name,
             recipe: action.recipe,
